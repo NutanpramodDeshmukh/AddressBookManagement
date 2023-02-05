29d2cbfdb3989708eb91dea156b4bebceff8d06c
NutanpramodDeshmukh <nutanpd17@gmail.com>
2023-02-05 11:13:31 +0530

    Added:Contacts

---
+++ AddressBookManagement/Program.cs
@@ -0,0 +1,28 @@
+﻿using AddressBookManagement.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookManagement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AddressBook address = new AddressBook();
+            Console.WriteLine("Enter 1 to Add a new contact");
+            int result = int.Parse(Console.ReadLine());
+            switch (result)
+            {
+                case 1: 
+                    address.AddContact(); 
+                    break;
+                default: 
+              
+                    break;
+            }
+        }
+    }
+}
---
+++ AddressBookManagement/Properties/AddressBook.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookManagement.Properties
+{
+    class AddressBook
+    {
+        public class Contact
+        {
+            public string FirstName;
+            public string LastName;
+            public string Address;
+            public string City;
+            public string State;
+            public long Pincode;
+            public long PhoneNumber;
+            public string Email;
+        }
+            List<Contact> contacts;
+            public long AddressBook()
+            {
+                contacts = new List<Contact>();
+            }
+            public void AddContact()
+            {
+                Console.Write("Enter FirstName: ");
+                string firstName = Console.ReadLine();
+                Console.Write("Enter lastName: ");
+                string lastName = Console.ReadLine();
+                Console.Write("Enter phoneNumber: ");
+                string phoneNumber = Console.ReadLine();
+                Console.Write("Enter city: ");
+                string city = Console.ReadLine();
+                Console.Write("Enter state: ");
+                string state = Console.ReadLine();
+                Console.Write("Enter zip: ");
+                string pincode = Console.ReadLine();
+                Console.Write("Enter address: ");
+                string address = Console.ReadLine();
+                Console.Write("Enter Email Adress: ");
+                string email = Console.ReadLine();
+                Contact contact = new Contact()
+                {
+                    FirstName = firstName,
+                    LastName = lastName,
+                    PhoneNumber = long.Parse(phoneNumber),
+                    City = city,
+                    State = state,
+                    Pincode = long.Parse(pincode),
+                    Address = address,
+                    Email = email
+                };
+                this.contacts.Add(contact);
+                Console.WriteLine("Contact added Sucesfully...!");
+            }
+        }
+    }
+
