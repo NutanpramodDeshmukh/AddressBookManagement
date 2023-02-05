310d14f3f8f47cc5bf26b166875ce5db5fd477f8
NutanpramodDeshmukh <nutanpd17@gmail.com>
2023-02-05 22:06:12 +0530

    Added:Edit Contacts

---
+++ AddressBookMangamentEding/Program.cs
@@ -0,0 +1,30 @@
+﻿using AddressBookMangamentEding.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookMangamentEding
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter A to Add new contact\nEnter E to edit a contact");
+            char ch = Console.ReadLine().ToUpper()[0];
+            AddressBooks addressBooks = new AddressBooks();
+            switch (ch)
+            {
+                case 'A':
+                    addressBooks.AddContact();
+                    break;
+                case 'E':
+                    addressBooks.EditContact();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ AddressBookMangamentEding/Properties/AddressBooks.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookMangamentEding.Properties
+{
+    internal class AddressBooks
+    {
+        List<Contact> contacts;
+        public AddressBooks()
+        {
+            contacts = new List<Contact>();
+        }
+        public void AddContact()
+        {
+            Contact contact = TakeInputsForContact();
+            this.contacts.Add(contact);
+            Console.WriteLine("Contact added Sucesfully...!");
+        }
+        public Contact TakeInputsForContact()
+        {
+            Console.Write("Enter FirstName: ");
+            string firstName = Console.ReadLine();
+            Console.Write("Enter lastName: ");
+            string lastName = Console.ReadLine();
+            Console.Write("Enter phoneNumber: ");
+            string phoneNumber = Console.ReadLine();
+            Console.Write("Enter city: ");
+            string city = Console.ReadLine();
+            Console.Write("Enter state: ");
+            string state = Console.ReadLine();
+            Console.Write("Enter zip: ");
+            string pincode = Console.ReadLine();
+            Console.Write("Enter address: ");
+            string address = Console.ReadLine();
+            Console.Write("Enter Email Adress: ");
+            string email = Console.ReadLine();
+            Contact contact = new Contact()
+            {
+                FirstName = firstName,
+                LastName = lastName,
+                PhoneNumber = long.Parse(phoneNumber),
+                City = city,
+                State = state,
+                Pincode = long.Parse(pincode),
+                Address = address,
+                Email = email
+            };
+            return contact;
+        }
+        public void EditContact()
+        {
+            Console.WriteLine("Enter the FistName of the contact to edit that contact");
+            string fName = Console.ReadLine();
+            Contact contact = null;
+            foreach (var con in contacts)
+            {
+                if (con.FirstName.Equals(fName))
+                {
+                    contact = con; break;
+                }
+            }
+            if (contact == null)
+            {
+                Console.WriteLine("there is no contact with given FirstName...!");
+                return;
+            }
+            contact = TakeInputsForContact();
+            for (int i = 0; i < contacts.Count; i++)
+            {
+                if (contacts[i].FirstName.Equals(fName))
+                {
+                    contacts[i] = contact; break;
+                }
+            }
+            Console.WriteLine("Contact Edited successfully");
+            return;
+        }
+    }
+}
